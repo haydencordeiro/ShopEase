cb1ce0bc3c50bc56bd90b42c0cc7a10f338159a9
Hayden Cordeiro <cordeirohayden@gmail.com>
2023-10-24 16:00:15 -0400

    implemented image picket

--- components/LoyaltyTask.js
+++ components/LoyaltyTask.js
@@ -1,11 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Dimensions } from 'react-native';
+import { StyleSheet, Text, View, Dimensions, TouchableOpacity } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
+import React, { useState, useEffect } from 'react';
+
 var width = Dimensions.get('window').width; 
 
 
 const LoyaltyTask = () => {
+
+
+  const [image, setImage] = useState(null);
+
+  const pickImage = async () => {
+    // No permissions request is necessary for launching the image library
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    console.log(result);
+
+    if (!result.canceled) {
+      setImage(result.assets[0].uri);
+    }
+  };
+
+
   return (
-    <View style={styles.container}>
+    <TouchableOpacity style={styles.container} onPress={pickImage}>
       <View style={styles.leftComponent}>
         <View style={styles.profileIcon}><Text>T</Text></View>
       </View>
@@ -13,7 +37,7 @@ const LoyaltyTask = () => {
       <Text>Task 1</Text>
       <Text>Upload Picture to get points</Text>
       </View>
-    </View>
+    </TouchableOpacity>
 
   );
 }
